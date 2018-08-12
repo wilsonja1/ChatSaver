b5e1aba38ff340bd1a27756042a368c8a8bebd4d
Jack Wilson <battlejackmaster@gmail.com>
2018-08-11 18:04:14 -0700

    Adding User class and db connection.

--- ChatSaver/User.cs
+++ ChatSaver/User.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace ChatSaver
 {
     class User
     {
+        public int UserID;
         public string UserName;
-        public string
+        public string OauthToken;
     }
 }
