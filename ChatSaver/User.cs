d8d14777639ee7bc84985c48afbdcca0ce608aa9
Jack Wilson <battlejackmaster@gmail.com>
2018-08-11 17:34:43 -0700

    Add project files.

---
+++ ChatSaver/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSaver
+{
+    class User
+    {
+        public string UserName;
+        public string
+    }
+}
