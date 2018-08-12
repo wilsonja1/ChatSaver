fb26d2969d830b399f448d3637729d9fd40cf5de
Jack Wilson <battlejackmaster@gmail.com>
2018-08-11 23:35:53 -0700

    Revert "Adding User class and db connection."
    This reverts commit b5e1aba38ff340bd1a27756042a368c8a8bebd4d.

--- ChatSaver/User.cs
+++ ChatSaver/User.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.Entity;
 
 namespace ChatSaver
 {
     class User
     {
-        public int UserID;
         public string UserName;
-        public string OauthToken;
+        public string
     }
 }
