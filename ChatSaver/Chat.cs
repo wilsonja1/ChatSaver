a7fff7beca5f4eaf41186e47d484477869320ca9
Jack Wilson <battlejackmaster@gmail.com>
2018-08-16 19:00:12 -0700

    Added chat model to save chats, ChatSave is model to read chats and save to db

---
+++ ChatSaver/Chat.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatSaver
+{
+    public class Chat
+    {
+        public int ChatId { get; set; }
+        public string Name { get; set; }
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+        public string Channel { get; set; }
+
+        public Chat(string name, string message,DateTime time,string channel)
+        {
+            Name = name;
+            Message = message;
+            Time = time;
+            Channel = channel;
+        }
+    }
+}
---
+++ ChatSaver/ChatSave.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatSaver
+{
+    class ChatSave
+    {
+        private Connection _irc;
+        private Thread Saver;
+        private Model db;
+
+        public ChatSave(Connection irc)
+        {
+            _irc = irc;
+            Saver = new Thread(new ThreadStart(this.Run));
+        }
+
+        public void Start()
+        {
+            Saver.IsBackground = true;
+            Saver.Start();
+        }
+
+        public void Run()
+        {
+            db = new Model();
+            while (true)
+            {
+                string x = _irc.ReadMessage();
+                string name = "";
+                string channel = "";
+                string message = "";
+
+                if (!x.Contains("battlejacklegend"))
+                {
+                    try { 
+                        name = x.Split(':')[1].Split('!')[0];
+                        channel = _irc.channel;
+                        message = x.Split(':')[2];
+                    }
+                    catch
+                    {
+
+                    }
+
+                    Chat y = new Chat(name, message, DateTime.Now, channel);
+        
+                    db.Chat.Add(y);
+                    db.SaveChangesAsync();
+                }
+
+            }
+        }
+    }
+}
